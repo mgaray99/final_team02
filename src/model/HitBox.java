af84d1dec1fe175167cb1463cb759f40cd902f6c
Ryan Krakower <rmk44@duke.edu>
2020-11-08 17:06:57 -0500

    made HitBox class

---
+++ src/model/HitBox.java
@@ -0,0 +1,57 @@
+package model;
+
+public class HitBox {
+
+  double xLeft;
+  double yTop;
+  int xSize;
+  int ySize;
+
+  public HitBox(double xLeft, double yTop, int xSize, int ySize) {
+    this.xLeft = xLeft;
+    this.xSize = xSize;
+    this.yTop = yTop;
+    this.ySize = ySize;
+  };
+
+  public double getXLeft() {
+    return xLeft;
+  }
+
+  public double getXRight() {
+    return xLeft + xSize;
+  }
+
+  public double getYTop() {
+    return yTop;
+  }
+
+  public double getYBottom() {
+    return yTop + ySize;
+  }
+
+  public void translateX(double deltaX) {
+    xLeft += deltaX;
+  }
+
+  public void translateY(double deltaY) {
+    yTop += deltaY;
+  }
+
+  public Enum checkCollision(HitBox otherBox) {
+    if (xLeft+xSize <= otherBox.getXLeft()) {
+      return CollisionDirections.RIGHT;
+    }
+    if (otherBox.getXRight() <= xLeft) {
+      return CollisionDirections.LEFT;
+    }
+    if (yTop+ySize <= otherBox.getYTop()) {
+      return CollisionDirections.BOTTOM;
+    }
+    if (otherBox.getYBottom() <= yTop) {
+      return CollisionDirections.TOP;
+    }
+    return CollisionDirections.NONE;
+  }
+
+}
